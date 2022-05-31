acc31440c4219bd5b583a0ea7952f0d4f37df656
ShrutikaK2911 <95906055+ShrutikaK2911@users.noreply.github.com>
2022-05-31 16:29:47 +0530

    Add files via upload

---
+++ user/includes/footer.php
@@ -0,0 +1,11 @@
+
+
+
+
+<!--- /footer-top ---->
+<!---copy-right ---->
+<div class="copy-right">
+	<div class="container">
+	<p class="wow zoomIn animated animated" data-wow-delay=".3s" style="visibility: visible; animation-delay: 0.3s; animation-name: zoomIn;">© 2022 HolidayEscape . All Rights Reserved </p>
+	</div>
+</div>
